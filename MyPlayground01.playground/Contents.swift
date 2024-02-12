780dffa10e29ce80234976933a1deebc657dfba8
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-02-12 17:28:05 +0700

    Swift รองรับการกําหนดประเภทข้อมูลพื้นฐานต่างๆ ดังนี้

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,11 +1,12 @@
 import UIKit
 
 /*
- ประกาศตัวแปรและค่าคงที่โดยระบุประเภทข้อมูลด้วย
+ Swift รองรับการกําหนดประเภทข้อมูลพื้นฐานต่างๆ ดังนี้
+ Int, UInt: แทนจํานวนเต็ม 32 หรือ 64 บิต
+ Float: แทนจํานวนจริง 32 บิต
+ Double: แทนจํานวนจริง 64 บิต
+ Bool: แทนค่าจริง/เท็จ
+ String: แทนข้อความ
+ Character: แทนอักขระเดี่ยว
+ Tuples: แทนกลุ่มค่าหลายค่าในตัวแปรเดียว
  */
-
-var apple, orange, grapes: String
-apple = "Apple"
-
-var cube, cone, rhombus: Double
-cube = 3.4
