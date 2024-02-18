9ad8867ce31bbb35a403b161241c12b8170a0b53
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-02-18 17:35:06 +0700

    อักขระพิเศษใน String

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,21 +1,21 @@
 import UIKit
 
 /*
- String Literals
+ อักขระพิเศษใน String
 
- String Literals คือ ชุดของอักขระที่ถูกกําหนดให้อยู่ในโค้ด โดยสามารถส่งค่าเป็นประโยค คํา หรือแค่รายการอักขระ แล้วครอบด้วยเครื่องหมายคําพูดคู่ """"
- 
- 
- นอกจากนี้ ยังสามารถใช้ Multiline String Literal แบบต่อเนื่องหลายบรรทัดได้ด้วย Syntax พิเศษแบบนี้:
+ String Literal รองรับการใช้อักขระพิเศษดังตารางต่อไปนี้:
 
+ \0 - null character
+ \ - backslash
+ \t - tab นิพจน์
+ \n - line feed (ขึ้นบรรทัดใหม่)
+ \r - carriage return
+ " - เครื่องหมายคําพูดคู่
+
+ ' - เครื่องหมายคําพูดเดี่ยว
+ \u{n} - Unicode character
  */
 
-let message = "ข้อความตัวอย่าง"
-
-// Multiline String Literal """  """
-let multillineString = """
-บรรทัดแรกของข้อความ
-บรรทัดที่สอง
-บรรทัดที่สาม
-"""
-print(multillineString)
+let message = "สวัสดี\nคุณสบายดีไหม"
+let dollarSign = "\u{24}"
+print(dollarSign)
