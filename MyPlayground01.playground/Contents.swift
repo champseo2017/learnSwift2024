22684b1615a5f84b200f8583c1960c95ebc0d39d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-02-26 16:29:16 +0700

    ความสามารถในการเปลี่ยนแปลงของคอลเลกชัน (Mutability)

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,17 +1,47 @@
 import UIKit
 
 /*
- คอลเล็กชันใน Swift คือชนิดข้อมูลที่ใช้ในการเก็บกลุ่มของข้อมูลเข้าด้วยกัน เช่น รายการของข้อมูลหรือคู่ค่า (key-value pair) ดังนี้
+ ความสามารถในการเปลี่ยนแปลงของคอลเลกชัน (Mutability) ใน Swift
+ ใน Swift การทําให้ Array, Set และ Dictionary เป็น Immutable Collection ตลอดเวลาถือเป็นแนวปฏิบัติที่ดี
+
+ การใช้ Immutable Collection จะช่วยเพิ่มประสิทธิภาพของคอลเลกชัน และทําให้การใช้หน่วยความจําและการทํางานมีประสิทธิภาพและคาดเดาได้มากขึ้น
+ 
+ - Mutable Collection คือ คอลเลกชันที่สามารถเปลี่ยนแปลงแก้ไขเนื้อหาภายในได้หลังจากสร้างแล้ว เช่น เพิ่ม ลบ หรือแก้ไขค่า
+ - Immutable Collection คือ คอลเลกชันที่ไม่สามารถเปลี่ยนแปลงแก้ไขได้อีก หลังจากสร้างแล้วค่าจะคงที่
+ ใน Swift จึงแนะนําให้ใช้ Immutable Collection เพื่อประสิทธิภาพสูงสุด
  */
 
-// 1. Array: เป็นกลุ่มข้อมูลที่เรียงต่อกันโดยมีลำดับ สามารถเก็บข้อมูลที่มีประเภทเดียวกันหลายตัวได้ เช่น จำนวนเต็ม หรือสตริง
-let numbers = [1, 2, 3, 4, 5]
-let names = ["Alice", "Bob", "Charlie"]
+// Mutable Collection
+var mutableArry = [1, 2, 3]
+mutableArry.append(4)
+// [1, 2, 3, 4]
 
-// 2. Set: เป็นกลุ่มข้อมูลที่ไม่มีลำดับและไม่มีค่าที่ซ้ำกัน ใช้เมื่อต้องการเก็บข้อมูลที่ไม่ต้องการให้มีค่าซ้ำซ้อน เช่น ชุดข้อมูลที่ไม่มีสมาชิกซ้ำกัน
-let uniqueNumbers: Set<Int> = [1, 2, 3, 4, 5]
-let uniqueCharacters: Set<Character> = ["a", "b", "c"]
+var mutableSet = Set([1, 2, 3])
+mutableSet.insert(4)
+// [4, 2, 1, 3]
 
-// 3.Dictionary: เป็นกลุ่มข้อมูลที่เก็บข้อมูลเป็นคู่ค่า (key-value pair) โดยมีค่า key แต่ละตัวต้องไม่ซ้ำกัน เช่น ข้อมูลที่ต้องการทำการจับคู่
-let ages = ["Alice": 30, "Bob": 25, "Charlie": 35]
-let fruitStocks = ["Apple": 100, "Banana": 50, "Orange": 75]
+var mutableDice = ["a": 1, "b": 2]
+mutableDice["c"] = 3
+// ["a": 1, "b": 2, "c": 3]
+
+// Immutable Collection
+let array1 = [1, 2, 3]
+// เพิ่มค่าโดยสร้าง Instance ใหม่
+let array2 = array1 + [4]
+// [1, 2, 3, 4]
+
+// Immutable Dictionary
+let dict1 = ["a": 1, "b": 2]
+// เพิ่มค่าโดยสร้าง Instance ใหม่
+let dict2 = dict1.merging(["a": 3]) {$1}
+// $1 คือ closure syntax ที่ใช้กําหนดการ Handle Conflict ในกรณีที่ Key ซ้ํากันใน Dictionary
+// การกําหนด Closure { $1 } จะเป็นการ แทน ค่า value ที่ถูกเพิ่มเข้าไปใหม่ใน key ที่มีอยู่แล้ว
+// merging จะทําการ merge dict1 กับ dict ที่ส่งเข้ามาเป็น parameter โดยไม่สนใจ order ของ key
+// ["a": 3, "b": 2]
+
+// Immutable Set
+let immutableSet = Set([1, 2, 3])
+
+// เพิ่มค่าโดยสร้าง Set ใหม่
+let newSet = immutableSet.union([4])
+print(newSet)
