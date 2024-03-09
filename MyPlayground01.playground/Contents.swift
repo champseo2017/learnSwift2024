5cf23a27199b2895c2e329a89c91fc7d64d9b3fd
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-09 15:41:50 +0700

    การกำหนดและเรียกใช้ฟังก์ชัน and พารามิเตอร์และค่าที่คืนค่า

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,28 +1,27 @@
 import UIKit
 
 /*
- Function Returning a Function
- 
- // _operation: (Int, Int) -> Int
- // หมายถึงพารามิเตอร์ของฟังก์ชันที่ชื่อว่า _operation ซึ่งเป็นฟังก์ชันที่รับค่า Int สองตัวและคืนค่าเป็น Int
-
- // ส่วนของ "_" (underscore) ที่อยู่หน้าชื่อ operation ใช้เพื่อบอกว่าเมื่อเรียกใช้ฟังก์ชัน applyOperation ไม่จำเป็นต้องระบุชื่อพารามิเตอร์
- // นี่เป็นการใช้งานที่เรียกว่า "Anonymous Parameter Names" ใน Swift ทำให้การเรียกใช้ฟังก์ชันสะอาดและเรียบง่ายขึ้น
- 
- // toOperand และ andOperand เป็นส่วนของการประกาศพารามิเตอร์ในฟังก์ชัน applyOperation
- // พวกมันเป็นชื่อที่ใช้เพื่ออธิบายวัตถุประสงค์ของพารามิเตอร์แต่ละตัวให้ชัดเจนขึ้น
+ การกำหนดและเรียกใช้ฟังก์ชัน
+ func และมีชื่อฟังก์ชัน, พารามิเตอร์, ประเภทการคืนค่า,
+ และประเภทการคืนค่าถูกบ่งบอกด้วยลูกศร ->
  */
 
-
-func applyOperation(_ operation: (Int, Int) -> Int, toOperand operandOne: Int, andOperand operandTwo: Int) -> Int {
-   return operation(operandOne, operandTwo)
+func meetToFriends(friendName: String) -> String {
+   let meetMessage = "Hey, " + friendName + "!"
+   return meetMessage
 }
 
-// ฟังก์ชัน add เป็นตัวอย่างของฟังก์ชันที่สามารถส่งเป็นอาร์กิวเมนต์ให้กับ _operation
-func add(a: Int, b: Int) -> Int {
-   return a + b
+print(meetToFriends(friendName: "Ashok"))
+print(meetToFriends(friendName: "Mohan"))
+
+// พารามิเตอร์และค่าที่คืนค่า
+func meetToFriends(friendName: String, isGreeted: Bool) -> String {
+   if isGreeted {
+      return "Hello again, \(friendName)"
+   } else {
+      return "Hey, \(friendName)"
+   }
 }
 
-// เรียกใช้ applyOperation โดยส่งฟังก์ชัน add เป็นอาร์กิวเมนต์ โดยไม่ต้องระบุชื่อพารามิเตอร์สำหรับ operation
-let result = applyOperation(add, toOperand: 5, andOperand: 10)
-print(result)
+print(meetToFriends(friendName: "Ram", isGreeted: true))
+// พิมพ์ - Hello again, Ram!
