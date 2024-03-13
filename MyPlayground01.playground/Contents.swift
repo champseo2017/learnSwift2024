39b95474fbebfe815378029de7f41dd6e5894e94
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-13 22:51:29 +0700

    Escaping closures

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,16 +1,25 @@
 import UIKit
 
 /*
- Closures
+ Escaping closures
+ Serial ใช้เมื่อต้องการควบคุมลำดับการทำงานหรือป้องกันการแข่งขันของข้อมูล
+ Concurrent ใช้เมื่อต้องการประมวลผลหลายงานพร้อมกันเพื่อเพิ่มความเร็วในการทำงาน
+ .main หมายถึง main dispatch queue ซึ่งเป็น serial queue ที่ใช้สำหรับการอัปเดต UI และการทำงานที่เกี่ยวข้องกับ UI
+ การใช้ @escaping มีประโยชน์ในสถานการณ์ที่คุณต้องการให้งานบางอย่างทำเมื่อเงื่อนไขหรือเหตุการณ์บางอย่างเกิดขึ้น ไม่ว่าจะเป็นการโหลดข้อมูลจากเครือข่าย, การรอผลลัพธ์จากการคำนวณที่ใช้เวลานาน, หรืออื่นๆ
  */
-// สร้างอาร์เรย์ของตัวเลข
-let numbers = [5, 3, 8, 7, 2]
-// ใช้ closure expression เพื่อเรียงลำดับตัวเลขจากน้อยไปมาก
-// (n1: Int, n2: Int) -> Bool เป็นตัวอย่างของ anonymous function ใน closure expression
-let sortedNumbers = numbers.sorted(by: { (n1: Int, n2: Int) -> Bool in
-   return n1 < n2
-})
-// in ใน closure expression ใช้แบ่งระหว่างการกำหนดพารามิเตอร์และประเภทของค่าที่จะคืนกลับกับตัวโค้ดที่จะทำงานจริงๆ 
 
-// แสดงผลลัพธ์
-print(sortedNumbers)
+func delayedPrint(message: String, delay: TimeInterval, completion: @escaping () -> Void) {
+   // รอเวลาที่กำหนด (เหมือนรอพิซซ่า)
+   DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+      // พิมพ์ข้อความ (เหมือนพิซซ่ามาถึง)
+      print(message)
+      // เรียกใช้งาน completion (เหมือนไปรับพิซซ่า)
+      completion()
+   }
+}
+
+// เรียกใช้ฟังก์ชัน
+delayedPrint(message: "Hello, World!", delay: 2.0, completion: {
+   // สิ่งที่ทำหลังจากพิมพ์ข้อความ (เหมือนกิจกรรมหลังจากรับพิซซ่า)
+   print("Printed after delay")
+})
