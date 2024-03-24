b0852194d4292abd57fad9ec2756458ab6fa5363
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-24 23:43:10 +0700

    อีนัมเมอเรชั่น

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,44 +1,17 @@
 import UIKit
 
 /*
- คลาส, โครงสร้าง, และอีนัมเมอเรชั่น
+ อีนัมเมอเรชั่น
 
  */
 
-// คลาส
-class Animal {
-   var name: String
-   var age: Int
-   
-   init(name: String, age: Int) {
-      self.name = name
-      self.age = age
-   }
-   
-   func description() -> String {
-      return "\(name) อายุ \(age) ปี"
-   }
+
+enum PointOfCompass {
+   case north
+   case south
+   case east
+   case west
 }
 
-// โครงสร้าง
-struct Coordinate {
-   var x: Double
-   var y: Double
-}
-
-// อีนัมเมอเรชั่น
-enum Direction {
-   case up
-   case down
-   case left
-   case right
-}
-
-// สร้างอินสแตนซ์ของคลาส
-let myPet = Animal(name: "Milo", age: 5)
-
-// สร้างอินสแตนซ์ของโครงสร้าง
-var point = Coordinate(x: 10.0, y: 20.0)
-
-// ใช้งานอีนัมเมอเรชั่น
-var moveDirection = Direction.up
+// เหมือนกับประเภทข้อมูลอื่นๆ ใน Swift, ชื่อของอีนัมเมอเรชั่นเริ่มต้นด้วยตัวพิมพ์ใหญ่
+var mainDirection = PointOfCompass.south
