dceaa39331ab3dcf3138983a19a9dedccbb248c3
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-25 00:03:24 +0700

    คำสั่ง `indirect` ใน Swift

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,17 +1,47 @@
 import UIKit
 
 /*
- อีนัมเมอเรชั่น
+ คำสั่ง `indirect` ใน Swift ใช้กับอีนัมเมอเรชั่นเพื่อระบุว่ากรณีของอีนัมเมอเรชั่นนั้น
 
  */
 
+// สามารถมีอินสแตนซ์ของอีนัมเมอเรชั่นเองเป็นค่าที่เกี่ยวข้องได้
+// ทำให้สามารถสร้างโครงสร้างข้อมูลแบบ Recursive ได้
 
-enum PointOfCompass {
-   case north
-   case south
-   case east
-   case west
+// ตัวอย่างการใช้งาน `indirect` ในอีนัมเมอเรชั่น
+
+
+enum ArithmeticExp {
+   // กรณีที่เป็นตัวเลข
+   case number(Int)
+   // กรณีที่เป็นการบวก โดยมีนิพจน์ทางคณิตศาสตร์สองนิพจน์เป็นค่าที่เกี่ยวข้อง
+   indirect case addition(ArithmeticExp, ArithmeticExp)
+   // กรณีที่เป็นการคูณ โดยมีนิพจน์ทางคณิตศาสตร์สองนิพจน์เป็นค่าที่เกี่ยวข้อง
+   indirect case multiplication(ArithmeticExp, ArithmeticExp)
 }
 
-// เหมือนกับประเภทข้อมูลอื่นๆ ใน Swift, ชื่อของอีนัมเมอเรชั่นเริ่มต้นด้วยตัวพิมพ์ใหญ่
-var mainDirection = PointOfCompass.south
+// เพื่อพิมพ์ค่าของ `exp`, คุณต้องใช้ฟังก์ชันเพื่อแปลงค่าจากอีนัมเมอเรชั่นเป็น String
+
+func evaluate(_ expression: ArithmeticExp) -> Int {
+    switch expression {
+       // ถ้านิพจน์เป็นตัวเลข, มันจะคืนค่าตัวเลขนั้นๆ
+          case .number(let value):
+              return value
+          // ถ้านิพจน์เป็นการบวก, มันจะเรียกฟังก์ชัน `evaluate` อีกครั้งสำหรับทั้งสองนิพจน์ที่เกี่ยวข้อง
+          // และบวกผลลัพธ์ของพวกเขา
+          case .addition(let left, let right):
+              return evaluate(left) + evaluate(right)
+          // ถ้านิพจน์เป็นการคูณ, มันจะเรียกฟังก์ชัน `evaluate` อีกครั้งสำหรับทั้งสองนิพจน์ที่เกี่ยวข้อง
+          // และคูณผลลัพธ์ของพวกเขา
+          case .multiplication(let left, let right):
+              return evaluate(left) * evaluate(right)
+    }
+}
+
+// ตัวอย่าง: สร้างนิพจน์ (1 + 2) * 3 โดยใช้อีนัมเมอเรชั่น
+let exp = ArithmeticExp.multiplication(.addition(.number(1), .number(2)), .number(3))
+// ในตัวอย่างนี้, `addition` และ `multiplication` มีค่าที่เกี่ยวข้องเป็นอีนัมเมอเรชั่นเดียวกัน
+// ทำให้สามารถสร้างนิพจน์ทางคณิตศาสตร์ที่ซับซ้อนได้
+
+// พิมพ์ผลลัพธ์ของนิพจน์
+print(evaluate(exp))
