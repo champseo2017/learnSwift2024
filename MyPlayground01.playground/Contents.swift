5b94df56ef94339935afb0dbeb3c32dcf9f52ee9
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-04-01 00:06:06 +0700

    คำสั่ง `indirect` 02

--- MyPlayground01.playground/Contents.swift
+++ MyPlayground01.playground/Contents.swift
@@ -1,47 +1,32 @@
 import UIKit
 
 /*
- คำสั่ง `indirect` ใน Swift ใช้กับอีนัมเมอเรชั่นเพื่อระบุว่ากรณีของอีนัมเมอเรชั่นนั้น
+ คำสั่ง `indirect`
+ การใส่ indirect ไว้ข้างหน้า enumeration case ใน ArithmeticExp ทำให้ case นั้นสามารถมี associated value ที่เป็น ArithmeticExp ได้ ซึ่งเป็นการอนุญาตให้เกิด recursion
 
  */
 
-// สามารถมีอินสแตนซ์ของอีนัมเมอเรชั่นเองเป็นค่าที่เกี่ยวข้องได้
-// ทำให้สามารถสร้างโครงสร้างข้อมูลแบบ Recursive ได้
-
-// ตัวอย่างการใช้งาน `indirect` ในอีนัมเมอเรชั่น
-
-
 enum ArithmeticExp {
-   // กรณีที่เป็นตัวเลข
    case number(Int)
-   // กรณีที่เป็นการบวก โดยมีนิพจน์ทางคณิตศาสตร์สองนิพจน์เป็นค่าที่เกี่ยวข้อง
    indirect case addition(ArithmeticExp, ArithmeticExp)
-   // กรณีที่เป็นการคูณ โดยมีนิพจน์ทางคณิตศาสตร์สองนิพจน์เป็นค่าที่เกี่ยวข้อง
    indirect case multiplication(ArithmeticExp, ArithmeticExp)
 }
 
-// เพื่อพิมพ์ค่าของ `exp`, คุณต้องใช้ฟังก์ชันเพื่อแปลงค่าจากอีนัมเมอเรชั่นเป็น String
+let five = ArithmeticExp.number(5)
+let four = ArithmeticExp.number(4)
+let sum = ArithmeticExp.addition(five, four)
+let product = ArithmeticExp.multiplication(sum, ArithmeticExp.number(2))
 
-func evaluate(_ expression: ArithmeticExp) -> Int {
-    switch expression {
-       // ถ้านิพจน์เป็นตัวเลข, มันจะคืนค่าตัวเลขนั้นๆ
-          case .number(let value):
-              return value
-          // ถ้านิพจน์เป็นการบวก, มันจะเรียกฟังก์ชัน `evaluate` อีกครั้งสำหรับทั้งสองนิพจน์ที่เกี่ยวข้อง
-          // และบวกผลลัพธ์ของพวกเขา
-          case .addition(let left, let right):
-              return evaluate(left) + evaluate(right)
-          // ถ้านิพจน์เป็นการคูณ, มันจะเรียกฟังก์ชัน `evaluate` อีกครั้งสำหรับทั้งสองนิพจน์ที่เกี่ยวข้อง
-          // และคูณผลลัพธ์ของพวกเขา
-          case .multiplication(let left, let right):
-              return evaluate(left) * evaluate(right)
-    }
+func evaluate (_ expression: ArithmeticExp) -> Int {
+   switch expression {
+   case let .number(value):
+      return value
+   case let .addition(left, right):
+      return evaluate(left) + evaluate(right)
+   case let .multiplication(left, right):
+      return evaluate(left) * evaluate(right)
+   }
 }
 
-// ตัวอย่าง: สร้างนิพจน์ (1 + 2) * 3 โดยใช้อีนัมเมอเรชั่น
-let exp = ArithmeticExp.multiplication(.addition(.number(1), .number(2)), .number(3))
-// ในตัวอย่างนี้, `addition` และ `multiplication` มีค่าที่เกี่ยวข้องเป็นอีนัมเมอเรชั่นเดียวกัน
-// ทำให้สามารถสร้างนิพจน์ทางคณิตศาสตร์ที่ซับซ้อนได้
-
-// พิมพ์ผลลัพธ์ของนิพจน์
-print(evaluate(exp))
+let result = evaluate(product)
+print(result)
